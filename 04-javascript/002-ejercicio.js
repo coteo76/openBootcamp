4d9e94015ea3ddc7df5963db4a3b2db4880824b7
coteo76 <oteo.carlos@gmail.com>
2022-04-07 15:07:40 +0200

    js-ejercicio 2

---
+++ 04-javascript/002-ejercicio.js
@@ -0,0 +1,11 @@
+// ejercicio 2
+
+var cadenaTexto = "Cadena inicial";
+
+function verCadena() {
+    var cadenaTexto = "Cadena dentro de función";
+    console.log(cadenaTexto);
+}
+
+verCadena();
+console.log(cadenaTexto);
