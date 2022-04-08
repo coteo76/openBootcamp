377869d09d1b17bc7893af5bd890cfd8ac84c125
coteo76 <oteo.carlos@gmail.com>
2022-04-08 08:12:22 +0200

    js sesion 4

---
+++ 04-javascript/ejercicio-41.js
@@ -0,0 +1,13 @@
+// sesion 4 - ejercicio 1
+
+// frase inicial
+var frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana.";
+
+// cambiar
+var strCambiar = "ventana";
+
+// por
+var strPor = "terraza";
+
+// ver resultado
+console.log(frase.replace(strCambiar, strPor));
---
+++ 04-javascript/ejercicio-42.js
@@ -0,0 +1,7 @@
+// sesion 4 - ejercicio 2
+
+// frase inicial
+var frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana.";
+
+// ver frase en mayusculas
+console.log(frase.toUpperCase());
