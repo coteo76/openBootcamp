11098bf110cbb4a7c7c3ed59668c3f02efedea29
coteo76 <oteo.carlos@gmail.com>
2022-04-11 08:23:46 +0200

    js ejercicio 6

--- 04-javascript/ejercicio-50.js
+++ 04-javascript/ejercicio-50.js
@@ -1,4 +1,4 @@
-// sesion 5 - ejercicio 1
+// sesion 5
 
 for( let i = 1; i <= 10; i++ ) {
     console.log( Math.round( Math.random() * 20 ) )
---
+++ 04-javascript/ejercicio-60.js
@@ -0,0 +1,10 @@
+// sesion 6
+
+// creamos array
+const lista = [];
+
+// llenamos array con los numeros del 1 al 20
+for( let i = 1; i <= 20; i++ ) { lista.push(i); }
+
+// mostramos los elementos del en consola
+lista.forEach( elemento => console.log(elemento) );
