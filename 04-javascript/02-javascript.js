c7da3da3752fc0cd8c1ac18a71c19bead20ba765
coteo76 <oteo.carlos@gmail.com>
2022-05-11 21:07:04 +0200

    js tema 2

---
+++ 04-javascript/02-javascript.js
@@ -0,0 +1,7 @@
+var alumno = [ "Carlos", 45, true, new Date("august 26 1976"), 
+            {   titulo: "Juego de Tronos",
+                autor: "George R. R. Martin",
+                fecha: new Date("1996"),
+                url: "https://es.wikipedia.org/wiki/Juego_de_tronos"  } ]
+
+console.log(alumno);
