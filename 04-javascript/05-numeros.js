ae11bce8acc09d0c107f023dac26907f8659f190
coteo76 <oteo.carlos@gmail.com>
2022-05-12 08:25:39 +0200

    js tema 5

---
+++ 04-javascript/05-numeros.js
@@ -0,0 +1,13 @@
+var alturaCms = 170;
+var pesoKg = 80.5;
+
+var alturaMt = alturaCms / 100;
+var alturaMtRed = Math.ceil(alturaMt);
+var pesoRed = Math.floor(pesoKg);
+
+var maximoValor = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
+
+console.log(alturaMt);
+console.log(alturaMtRed);
+console.log(pesoRed);
+console.log(maximoValor);
