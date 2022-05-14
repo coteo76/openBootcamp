c6bc5c436cb9b2c6606f9f6445abde139b6c1a8b
coteo76 <oteo.carlos@gmail.com>
2022-05-14 17:22:01 +0200

    js tema 8

---
+++ 04-javascript/08-funciones.js
@@ -0,0 +1,32 @@
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una función sin parámetros que devuelva siempre "true"
+const funcionVacia() {
+    return true;
+}
+funcionVacia();
+
+// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+const funcionAsincrona = new Promise(function(resolve, reject) {
+    setTimeout(function() { resolve("Hola, soy una promesa.") }, 3000 );
+});
+
+funcionAsincrona.then(function(value) {
+    console.log(value);
+});
+
+// - Una función generadora de índices pares automáticos
+function* generaIndicaPar() {
+    let index = 0;
+    while (true) {
+        index+=2;
+        yield index;
+    }
+}
+
+const gen = generaIndicaPar()
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+
+
